74d7f076c08ddd9e4f39c93a7b7793961907e1bd
sofiafernandezmoreno <chui274@gmail.com>
2019-10-22 22:36:27 +0200

    cahnges in web

---
+++ assets/js/typescript.js
@@ -0,0 +1,6 @@
+new TypeIt('#replaceStrings', {
+    strings: ["I'm a computer science, specialized in computer engineering.", "But right now, I'm working of DevOps Junior on Loycus."],
+    speed: 75,
+    breakLines: false,
+    waitUntilVisible: true
+  }).go();
